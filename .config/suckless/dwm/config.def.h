2f1042fd329701977ff15fd4d447b3a6e37d7657
DILPREET1910 <dsahney1910@gmail.com>
2024-07-12 18:56:20 +0530

    dwm reload and quite keybinds added

--- .config/suckless/dwm/config.def.h
+++ .config/suckless/dwm/config.def.h
@@ -99,7 +99,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
 };
 
 /* button definitions */
