80f28d2fc8068ea0b39bd2ced04c7314d5de972c
DILPREET1910 <dsahney1910@gmail.com>
2024-08-13 22:16:08 +0530

    slock message patch

--- .config/suckless/slock/config.def.h
+++ .config/suckless/slock/config.def.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "dilpreet";
+static const char *group = "dilpreet";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
@@ -10,3 +10,12 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x13";
