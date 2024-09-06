d93daba45be62787ada148214920520a97e5f5f9
DILPREET1910 <dsahney1910@gmail.com>
2024-09-06 15:14:47 +0530

    proper config.def.h file

--- .config/suckless/dwm/config.def.h
+++ .config/suckless/dwm/config.def.h
@@ -1,22 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono:size=10" };
-static const char dmenufont[]       = "JetBrainsMono:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Ubuntu:size=12" };
+static const char dmenufont[]       = "Ubuntu:size=12";
+static const char col_gray1[]       = "#181926";
+static const char col_gray2[]       = "#24273a";
+static const char col_gray3[]       = "#cad3f5";
+static const char col_gray4[]       = "#c6a0f6";
+static const char col_cyan[]        = "#313244";
+static const char border_cyan[]        = "#8aadf4";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  border_cyan  },
 };
 
 /* tagging */
@@ -60,7 +61,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "20", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st","-e","/usr/bin/tmux", NULL };
 
 static const Key keys[] = {
