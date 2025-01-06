54c8938d9cedb199a14c2dbabbcb1cd4984327e4
DILPREET1910 <dsahney1910@gmail.com>
2025-01-06 09:42:43 +0530

    disable copilot for leetcode dir

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,7 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
   pattern = {
     '/home/dilpreet/Documents/MyPrograms/java/codechef/*',
     '/home/dilpreet/Documents/MyPrograms/java/leetcode-150/*',
+    '/home/dilpreet/Documents/MyPrograms/java/algomaster/*',
   },
   command = "Copilot disable"
 })
